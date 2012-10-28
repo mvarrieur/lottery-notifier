9fe245cf68145be7e683205c07ff5679fe18fd14
Michael Varrieur <mike.varrieur@gmail.com>
2012-10-28 14:10:28 -0400

    Change some controllers to take an API key

--- app/controllers/lottery_numbers_controller.rb
+++ app/controllers/lottery_numbers_controller.rb
@@ -34,54 +34,39 @@ class LotteryNumbersController < ApplicationController
     @lottery_number = LotteryNumber.find(params[:id])
   end
 
-  def create
-    @lottery_number = LotteryNumber.new(params[:lottery_number])
+  def create(api_key = '')
+    if (api_key == ENV['API_KEY'])
+      @lottery_number = LotteryNumber.new(params[:lottery_number])
 
-    respond_to do |format|
-      if @lottery_number.save
-        format.html { redirect_to @lottery_number, notice: 'Lottery number was successfully created.' }
-        format.json { render json: @lottery_number, status: :created, location: @lottery_number }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @lottery_number.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @lottery_number.save
+          format.html { redirect_to @lottery_number, notice: 'Lottery number was successfully created.' }
+          format.json { render json: @lottery_number, status: :created, location: @lottery_number }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @lottery_number.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      render :file => "public/401.html", :status => :unauthorized
     end
   end
 
-  def update
-    @lottery_number = LotteryNumber.find(params[:id])
+  def update(api_key = '')
+    if (api_key == ENV['API_KEY'])
+      @lottery_number = LotteryNumber.find(params[:id])
 
-    respond_to do |format|
-      if @lottery_number.update_attributes(params[:lottery_number])
-        format.html { redirect_to @lottery_number, notice: 'Lottery number was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @lottery_number.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @lottery_number.update_attributes(params[:lottery_number])
+          format.html { redirect_to @lottery_number, notice: 'Lottery number was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: "edit" }
+          format.json { render json: @lottery_number.errors, status: :unprocessable_entity }
+        end
       end
-    end
-  end
-
-  def destroy
-    @lottery_number = LotteryNumber.find(params[:id])
-    @lottery_number.destroy
-
-    respond_to do |format|
-      format.html { redirect_to lottery_numbers_url }
-      format.json { head :no_content }
-    end
-  end
-
-  def get_latest
-    @saved = LotteryNumber.get_latest
-
-    respond_to do |format|
-      if @saved
-        format.html { redirect_to action: 'email_winner', controller: 'users' }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @saved.errors, status: :unprocessable_entity }
-      end
+    else
+      render :file => "public/401.html", :status => :unauthorized
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 
 #pick = Pick.create(pick_number: '9-5-5-0')
 user = User.create!(name: 'Mike Varrieur', email: 'Mikey7047@gmail.com')
-user.picks.create!(pick_number: '9-5-5-0')
+user.picks.create!(pick_number: '4-9-8-1')
