2c4f7eefe53e31ea275f8229e18f49b670443f5b
Michael Varrieur <mike.varrieur@gmail.com>
2012-10-29 20:12:36 -0400

    Add precompiled asset

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,7 @@ module LotteryApp
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
     config.assets.compile = true
+    config.assets.precompile += 'html5shiv.js'
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
